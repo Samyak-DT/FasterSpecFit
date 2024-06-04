7e1e523ee89fe0b70de443ab1e60d5c9e15a5b1f
John Moustakas <john.moustakas@gmail.com>
2024-06-04 07:55:43 -0700

    move build_test_data code to another routine

---
+++ build_test_data.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+"""
+Build the test dataset (will only work at NERSC if run by Moustakas).
+
+"""
+def build_test_data(datadir='./', ntargets=None):
+    """Build the test dataset; will only work at NERSC if run by Moustakas.
+
+    """
+    import os
+    import numpy as np
+    import fitsio
+    from astropy.table import Table
+    from desiutil.dust import dust_transmission
+    from desispec.io import read_spectra, write_spectra
+    from desispec.io.util import replace_prefix    
+    from desitarget import geomask
+    from fastspecfit.io import write_fastspecfit
+
+    from desiutil.log import get_logger, INFO
+    log = get_logger(INFO)
+
+    out_coaddfile = os.path.join(datadir, 'coadd-test-data2.fits')
+    out_redrockfile = os.path.join(datadir, 'redrock-test-data2.fits')
+    out_fastfile = os.path.join(datadir, 'fastspec-test-data2.fits')
+    if os.path.isfile(out_coaddfile):
+        log.info(f'Warning: overwriting existing output file {out_coaddfile}')
+
+    # select spectra with strong line-emission and no broad lines (for now)
+    specprod, coadd_type = 'iron', 'healpix'
+    survey, program, healpix = 'sv1', 'bright', 5060
+    
+    coaddfile = os.path.join(os.getenv('DESI_ROOT'), 'spectro', 'redux', specprod, coadd_type, survey, program,
+                             str(healpix//100), str(healpix), f'coadd-{survey}-{program}-{healpix}.fits')
+    redrockfile = replace_prefix(coaddfile, 'coadd', 'redrock')
+        
+    fastfile = os.path.join(os.getenv('DESI_ROOT'), 'spectro', 'fastspecfit', specprod, coadd_type, survey, program,
+                             str(healpix//100), str(healpix), f'fastspec-{survey}-{program}-{healpix}.fits.gz')
+    fast = Table(fitsio.read(fastfile, 'FASTSPEC', columns=['TARGETID', 'Z', 'HALPHA_EW', 'HALPHA_EW_IVAR', 'HALPHA_BROAD_FLUX_IVAR']))
+    meta = Table(fitsio.read(fastfile, 'METADATA', columns=['Z', 'Z_RR']))
+
+    if False:
+        I = np.where((fast['HALPHA_EW'] > 5.) * (fast['HALPHA_EW'] * np.sqrt(fast['HALPHA_EW_IVAR']) > 5.) *
+                     (fast['HALPHA_BROAD_FLUX_IVAR'] == 0.) * (meta['Z'] == meta['Z_RR']))[0]
+    else:
+        I = np.arange(len(fast))
+    if ntargets is not None and ntargets <= len(I):
+        I = I[:ntargets]
+    nobj = len(I)
+
+    fast = Table(fitsio.read(fastfile, 'FASTSPEC', rows=I))
+    meta = Table(fitsio.read(fastfile, 'METADATA', rows=I))
+    targetids = meta['TARGETID'].data
+
+    write_fastspecfit(fast, meta, outfile=out_fastfile, coadd_type=coadd_type, specprod=specprod)
+
+    # read the best-fitting continuum models
+    models, hdr = fitsio.read(fastfile, 'MODELS', header=True)
+
+    modelwave = hdr['CRVAL1'] + np.arange(hdr['NAXIS1']) * hdr['CDELT1']
+    models = np.squeeze(models[I, :, :])
+    continuum = np.squeeze(models[:, 0, :] + models[:, 1, :])
+
+    # read the original data (spectra) and redshifts of the selected targets
+    spec = read_spectra(coaddfile).select(targets=targetids)
+    assert(np.all(spec.target_ids() == targetids))    
+
+    alltargetids = fitsio.read(redrockfile, ext='REDSHIFTS', columns='TARGETID')
+    rows = np.where(np.isin(alltargetids, targetids))[0]
+    zb = fitsio.read(redrockfile, ext='REDSHIFTS', rows=rows)
+    fm = fitsio.read(redrockfile, ext='FIBERMAP', rows=rows)
+    
+    I = geomask.match_to(zb['TARGETID'], targetids)
+    zb = zb[I]
+    fm = fm[I]
+    assert(np.all(zb['TARGETID'] == targetids))
+    assert(np.all(fm['TARGETID'] == targetids))
+        
+    # Correct for dust extinction and subtract the stellar
+    # continuum. Unfortunately, dust_transmission isn't vectorized, so we might
+    # as well loop.
+    for iobj in range(nobj):
+        for cam in spec.bands:
+            mw_transmission_spec = dust_transmission(spec.wave[cam], meta[iobj]['EBV'])
+            spec.flux[cam][iobj, :] /= mw_transmission_spec
+            spec.ivar[cam][iobj, :] *= mw_transmission_spec**2
+
+            # this isn't quite right but shouldn't matter for these tests
+            spec.flux[cam][iobj, :] -= np.interp(spec.wave[cam], modelwave, continuum[iobj, :])
+
+    # write out
+    log.info(f'Writing {len(targetids)} targets to {out_coaddfile}')
+    write_spectra(out_coaddfile, spec)
+
+    log.info(f'Writing {len(targetids)} targets to {out_redrockfile}')
+    fitsio.write(out_redrockfile, zb, extname='REDSHIFTS', clobber=True)
+    fitsio.write(out_redrockfile, fm, extname='FIBERMAP')
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--datadir', type=str, default="./data", help='I/O directory.')
+    parser.add_argument('--ntargets', type=int, default=None, help='For testing, test on ntargets objects.')
+    args = parser.parse_args()
+
+    build_test_data(args.datadir, ntargets=args.ntargets)
--- fastspec_emlines.py
+++ fastspec_emlines.py
@@ -14,7 +14,6 @@ Dependencies:
 * desimodel
 * fastspecfit
 
-time python code/desihub/FasterSpecFit/FasterSpecFit/fastspec_emlines.py --build-test-data --ntargets=20
 time python code/desihub/FasterSpecFit/FasterSpecFit/fastspec_emlines.py
 time python code/desihub/FasterSpecFit/FasterSpecFit/fastspec_emlines.py --fast
 
@@ -27,7 +26,7 @@ log = get_logger(INFO)
 
 from FasterSpecFit import ResMatrix
 
-def read_test_data(datadir='.'):
+def read_test_data(datadir='.', ntargets=None):
     """Read the test data.
 
     """
@@ -42,9 +41,9 @@ def read_test_data(datadir='.'):
     CTools = ContinuumTools()
         
     # read the data - fastspecfit.io.DESISpectra().select()
-    coaddfile = os.path.join(datadir, 'coadd-test-data.fits')
-    redrockfile = os.path.join(datadir, 'redrock-test-data.fits')
-    #fastfile = os.path.join(datadir, 'fastspec-test-data.fits')
+    coaddfile = os.path.join(datadir, 'coadd-test-data2.fits')
+    redrockfile = os.path.join(datadir, 'redrock-test-data2.fits')
+    #fastfile = os.path.join(datadir, 'fastspec-test-data2.fits')
 
     spec = read_spectra(coaddfile)
 
@@ -53,6 +52,9 @@ def read_test_data(datadir='.'):
     assert(np.all(zb['TARGETID'] == fm['TARGETID']))
     zb.remove_column('TARGETID')
     meta = hstack((fm, zb))#, tsnr2))    
+
+    if ntargets is not None and ntargets <= len(meta):
+        meta = meta[:ntargets]
 
     # pack into a dictionary - fastspecfit.io.DESISpectra().read_and_unpack()
     
@@ -455,14 +457,14 @@ def drop_params(parameters, emfit, linemodel, Ifree):
     #          forbidden lines!
 
         
-def fit_emlines(datadir='.', fast=False):
+def fit_emlines(datadir='.', fast=False, ntargets=None):
     """Use the current (main) version of the emission-line fitting code.
 
     """
     from fastspecfit.emlines import EMFitTools
 
     t0 = time.time()    
-    specdata = read_test_data(datadir=datadir)
+    specdata = read_test_data(datadir=datadir, ntargets=ntargets)
     t1 = time.time()
     log.info(f'Gathering the data took {t1-t0:.2f} seconds.')
 
@@ -528,90 +530,6 @@ def fit_emlines(datadir='.', fast=False):
         # write out...
 
     
-def build_test_data(datadir='./', ntargets=None):
-    """Build the test dataset; will only work at NERSC if run by Moustakas.
-
-    """
-    import fitsio
-    from astropy.table import Table
-    from desiutil.dust import dust_transmission
-    from desispec.io import read_spectra, write_spectra
-    from desispec.io.util import replace_prefix    
-    from desitarget import geomask
-    from fastspecfit.io import write_fastspecfit
-
-    out_coaddfile = os.path.join(datadir, 'coadd-test-data.fits')
-    out_redrockfile = os.path.join(datadir, 'redrock-test-data.fits')
-    out_fastfile = os.path.join(datadir, 'fastspec-test-data.fits')
-    if os.path.isfile(out_coaddfile):
-        log.info(f'Warning: overwriting existing output file {out_coaddfile}')
-
-    # select spectra with strong line-emission and no broad lines (for now)
-    specprod, coadd_type = 'iron', 'healpix'
-    survey, program, healpix = 'sv1', 'bright', 5060
-    
-    coaddfile = os.path.join(os.getenv('DESI_ROOT'), 'spectro', 'redux', specprod, coadd_type, survey, program,
-                             str(healpix//100), str(healpix), f'coadd-{survey}-{program}-{healpix}.fits')
-    redrockfile = replace_prefix(coaddfile, 'coadd', 'redrock')
-        
-    fastfile = os.path.join(os.getenv('DESI_ROOT'), 'spectro', 'fastspecfit', specprod, coadd_type, survey, program,
-                             str(healpix//100), str(healpix), f'fastspec-{survey}-{program}-{healpix}.fits.gz')
-    fast = Table(fitsio.read(fastfile, 'FASTSPEC', columns=['TARGETID', 'Z', 'HALPHA_EW', 'HALPHA_EW_IVAR', 'HALPHA_BROAD_FLUX_IVAR']))
-    meta = Table(fitsio.read(fastfile, 'METADATA', columns=['Z', 'Z_RR']))
-
-    I = np.where((fast['HALPHA_EW'] > 5.) * (fast['HALPHA_EW'] * np.sqrt(fast['HALPHA_EW_IVAR']) > 5.) *
-                 (fast['HALPHA_BROAD_FLUX_IVAR'] == 0.) * (meta['Z'] == meta['Z_RR']))[0]
-    if ntargets is not None and ntargets <= len(I):
-        I = I[:ntargets]
-    nobj = len(I)
-
-    fast = Table(fitsio.read(fastfile, 'FASTSPEC', rows=I))
-    meta = Table(fitsio.read(fastfile, 'METADATA', rows=I))
-    targetids = meta['TARGETID'].data
-
-    write_fastspecfit(fast, meta, outfile=out_fastfile, coadd_type=coadd_type, specprod=specprod)
-
-    # read the best-fitting continuum models
-    models, hdr = fitsio.read(fastfile, 'MODELS', header=True)
-
-    modelwave = hdr['CRVAL1'] + np.arange(hdr['NAXIS1']) * hdr['CDELT1']
-    models = np.squeeze(models[I, :, :])
-    continuum = np.squeeze(models[:, 0, :] + models[:, 1, :])
-
-    # read the original data (spectra) and redshifts of the selected targets
-    spec = read_spectra(coaddfile).select(targets=targetids)
-    assert(np.all(spec.target_ids() == targetids))    
-
-    alltargetids = fitsio.read(redrockfile, ext='REDSHIFTS', columns='TARGETID')
-    rows = np.where(np.isin(alltargetids, targetids))[0]
-    zb = fitsio.read(redrockfile, ext='REDSHIFTS', rows=rows)
-    fm = fitsio.read(redrockfile, ext='FIBERMAP', rows=rows)
-    
-    I = geomask.match_to(zb['TARGETID'], targetids)
-    zb = zb[I]
-    fm = fm[I]
-    assert(np.all(zb['TARGETID'] == targetids))
-    assert(np.all(fm['TARGETID'] == targetids))
-        
-    # Correct for dust extinction and subtract the stellar
-    # continuum. Unfortunately, dust_transmission isn't vectorized, so we might
-    # as well loop.
-    for iobj in range(nobj):
-        for cam in spec.bands:
-            mw_transmission_spec = dust_transmission(spec.wave[cam], meta[iobj]['EBV'])
-            spec.flux[cam][iobj, :] /= mw_transmission_spec
-            spec.ivar[cam][iobj, :] *= mw_transmission_spec**2
-
-            # this isn't quite right but shouldn't matter for these tests
-            spec.flux[cam][iobj, :] -= np.interp(spec.wave[cam], modelwave, continuum[iobj, :])
-
-    # write out
-    log.info(f'Writing {len(targetids)} targets to {out_coaddfile}')
-    write_spectra(out_coaddfile, spec)
-
-    log.info(f'Writing {len(targetids)} targets to {out_redrockfile}')
-    fitsio.write(out_redrockfile, zb, extname='REDSHIFTS', clobber=True)
-    fitsio.write(out_redrockfile, fm, extname='FIBERMAP')
 
 
 def main():
@@ -621,18 +539,12 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--build-test-data', action='store_true', help='Build the test dataset.')
     parser.add_argument('--fast', action='store_true', help='Build the refactored, fast emission-line fitting code.')
     parser.add_argument('--datadir', type=str, default="./data", help='I/O directory.')
     parser.add_argument('--ntargets', type=int, default=None, help='For testing, test on ntargets objects.')
     args = parser.parse_args()
 
-    # Build the test dataset (will only work at NERSC if run by Moustakas).
-    if args.build_test_data:
-        build_test_data(args.datadir, ntargets=args.ntargets)
-        return
-    
-    fit_emlines(datadir=args.datadir, fast=args.fast)
+    fit_emlines(datadir=args.datadir, fast=args.fast, ntargets=args.ntargets)
         
 
 if __name__ == '__main__':
